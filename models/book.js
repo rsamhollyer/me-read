996c1a844fb95e3520f9d3f49ae2c8df2916286d
rsamhollyer <rsamhollyer@gmail.com>
2021-01-07 15:19:19 -0600

    update models to fix foreign keys

--- models/book.js
+++ models/book.js
@@ -12,7 +12,7 @@ module.exports = (sequelize, DataTypes) => {
     static associate(models) {
       // define association here
       Book.belongsTo(models.Author, {
-        foreignKey:"AuthorId"
+        foreignKey: "AuthorId"
       })
     }
   };
