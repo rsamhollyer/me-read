19518687167757dbe7cf86357af4469e4d349ce4
rsamhollyer <rsamhollyer@gmail.com>
2021-01-07 11:39:38 -0600

    adding utility file

---
+++ utils.js
@@ -0,0 +1,18 @@
+const layout = {
+    partials:{
+        header:"partials/header.html",
+        footer:"partials/footer.html", 
+        }
+}
+
+const navLayout = {
+    partials:{
+        header:"partials/navheader.html",
+        footer:"partials/footer.html"
+    }
+}
+
+module.exports = {
+    layout,
+    navLayout
+}
