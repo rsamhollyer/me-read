4825e086d00fdb2b6d8f7a704442ebdec2674f6f
rsamhollyer <rsamhollyer@gmail.com>
2021-01-07 11:39:58 -0600

    adding the remaining files to start the app

---
+++ migrations/20210107173222-create-author.js
@@ -0,0 +1,33 @@
+'use strict';
+module.exports = {
+  up: async (queryInterface, Sequelize) => {
+    await queryInterface.createTable('Authors', {
+      id: {
+        allowNull: false,
+        autoIncrement: true,
+        primaryKey: true,
+        type: Sequelize.INTEGER
+      },
+      UserId: {
+        type: Sequelize.INTEGER
+      },
+      authorfirst: {
+        type: Sequelize.STRING
+      },
+      authorlast: {
+        type: Sequelize.STRING
+      },
+      createdAt: {
+        allowNull: false,
+        type: Sequelize.DATE
+      },
+      updatedAt: {
+        allowNull: false,
+        type: Sequelize.DATE
+      }
+    });
+  },
+  down: async (queryInterface, Sequelize) => {
+    await queryInterface.dropTable('Authors');
+  }
+};
