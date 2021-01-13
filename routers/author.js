e39b708374b607bb8d5add4bade2eb2c61aaf6f7
rsamhollyer <rsamhollyer@gmail.com>
2021-01-13 08:32:43 -0600

    updated router to include a variable parameter that would sort the authors page in one get request

--- routers/author.js
+++ routers/author.js
@@ -5,13 +5,12 @@ const {
       authorControllers
 } = require("../controllers")
 
-router.get("/main", authorControllers.authorPage)
+router.get("/sort/main/:sorted", authorControllers.authorPage)
       .get("/add", authorControllers.renderAuthorForm)
       .post("/add", authorControllers.processNewAuthor)
       .delete("/:authorid",authorControllers.deleteAuthor)
       .put("/:authorid",authorControllers.editAuthor)
-      .get("/sort/main/az",authorControllers.descendingSort)
-      .get("/sort/main/za",authorControllers.ascendingSort)
+   
 
 
 module.exports = router
